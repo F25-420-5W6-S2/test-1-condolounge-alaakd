513fa2f3a5b93963e308740db8201ea06c1df6be
2363678 <2363678@JAC-8XNQRT3>
2025-11-18 11:34:51 -0500

    test #2

--- CondoLounge/Program.cs
+++ CondoLounge/Program.cs
@@ -1,4 +1,16 @@
+
+
+
+using CondoLounge.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
